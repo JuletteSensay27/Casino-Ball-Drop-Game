b3b1efecf762636f127bb368ba94213e3e95c0c5
Julette Peque <22-0200c@sgen.edu.ph>
2022-11-03 22:38:00 +0800

    Finished initBoard() Method and Generated Game Board UI

--- Casino-Ball-Drop-Game/Casino-Ball-Drop-Game/MainWindow.xaml.cs
+++ Casino-Ball-Drop-Game/Casino-Ball-Drop-Game/MainWindow.xaml.cs
@@ -20,21 +20,56 @@ namespace Casino_Ball_Drop_Game
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Label[][] gameBoard = new Label[10][]; 
+        private Label[][] gameBoard = new Label[7][]; 
         private Grid gameBoardCont = new Grid();
 
         public MainWindow()
         {
             InitializeComponent();
+            initBoard();
         }
 
         private void initBoard() 
         {
+            int v = 5;
+            int h = 5;
+            
+
             gameBoardCont = new Grid();
             gameBoardCont.VerticalAlignment = VerticalAlignment.Top;
             gameBoardCont.HorizontalAlignment = HorizontalAlignment.Left;
-            gameBoardCont.Width = 800;
-            gameBoardCont.Height = 800;
+            gameBoardCont.Width = 350;
+            gameBoardCont.Height = 350;
+            gameBoardCont.Margin = new Thickness(225,45,0,0);
+
+            mainGrid.Children.Add(gameBoardCont);
+
+            for (int i = 0; i < gameBoard.Length; i++)
+                gameBoard[i] = new Label[5];
+
+            for (int x = 0; x < gameBoard.Length; x++) 
+            {
+                for (int y = 0; y < gameBoard[x].Length; y++) 
+                {
+                    Label boardGrid = new Label();
+                    boardGrid.HorizontalAlignment = HorizontalAlignment.Left;   
+                    boardGrid.VerticalAlignment = VerticalAlignment.Top;
+                    boardGrid.Width = 40;
+                    boardGrid.Height = 40;
+                    boardGrid.Margin = new Thickness(h,v,0,0);
+                    boardGrid.BorderBrush = new SolidColorBrush(Colors.Black);
+                    boardGrid.BorderThickness = new Thickness(2, 2, 2, 2);
+                    gameBoard[x][y] = boardGrid;
+                    gameBoardCont.Children.Add(gameBoard[x][y]);
+
+                    h += 5 + (int)boardGrid.Width;
+                }
+                h = 5;
+                v += 45;
+            }
+            
+
+            
         }
     }
 }
